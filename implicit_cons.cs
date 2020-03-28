0c5f67bc491ce66dfa8b1f1acc7bf6807b225ecd
AHMAD-KABEER <52852345+Ahmad-kabeer@users.noreply.github.com>
2020-03-28 16:48:32 +0530

    code of implicit constructor.

---
+++ implicit_cons.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class SecondClass
+    {
+        /// this program is to learn imlicit constructor which is known as parameterless constructor and it is public constructor.
+        int a; 
+        bool b;
+        double d;
+        static void Main(string[] args)
+        {
+            SecondClass obj = new SecondClass();
+
+            Console.WriteLine("value of b " + obj.a);
+            Console.WriteLine("value of b " + obj.b);
+            Console.WriteLine("value of d " + obj.d);
+            Console.ReadLine();
+        }
+    }
+}
