635cb36af94ff76d8d156e1e16e41d687605358d
AHMAD-KABEER <52852345+Ahmad-kabeer@users.noreply.github.com>
2020-03-28 17:05:00 +0530

    code of explicit constructor.

---
+++ explicit_cons.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class explicit_cons
+    {
+        public explicit_cons()      ///defining constructor
+        {
+            Console.WriteLine("this explicit constructor is called");
+        }
+        static void Main(string[] arg)
+        {
+            explicit_cons obj1 = new explicit_cons();    ///number of object is equal to no. of calling.
+            explicit_cons obj2 = new explicit_cons();
+            Console.ReadLine();
+        }
+    }
+}
