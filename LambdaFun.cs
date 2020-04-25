ac2f734e89c114316a779cd0e29821e28b77e539
AHMAD-KABEER <ahmad.kabeer244@gmail.com>
2020-04-25 12:40:22 +0530

    Lambda function in C#.
    Well define code of Lambda function in C#.

---
+++ LambdaFun.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    public delegate string DelLambda(string a);
+    class LambdaFun
+    {
+
+        static void Main()
+        {
+            DelLambda obj = (name) =>
+            {
+                return "hello " + name + " is a good men.";
+            };
+            var b = obj.Invoke("khansab");
+            Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+}
