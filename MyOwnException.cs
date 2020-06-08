681ea0b52d72eaefd2cfc61c0879a818ea234135
AHMAD-KABEER <ahmad.kabeer244@gmail.com>
2020-06-08 17:59:57 +0530

    well define code of how to define own exception and how to use it.
    well define code of how to define own exception(application exception) and how to use it(how ti throw it).

---
+++ MyOwnException.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class MyOwnException : ApplicationException
+    {
+        public override string Message
+        {
+            get
+            {
+                return "attempted to divide by odd number";
+            }
+        }
+    }
+    public class demo
+    {
+        static void Main()
+        {
+            Console.WriteLine("enter 1st number");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("enter 2nd number");
+            int b = Int32.Parse(Console.ReadLine());
+            if (b % 2 > 0)
+            {
+                throw new MyOwnException();
+                //          OR
+                //MyOwnException ex1 = new MyOwnException();
+                //throw ex1;
+            }
+            int c = a / b;
+            Console.WriteLine("result is = " + c);
+            Console.ReadLine();
+        }
+    }
+}
+
